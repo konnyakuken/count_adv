593d492c10cf0191dcba86e9d95c4451a444bbb2
konnyakuken <t0516wakamiya0402@icloud.com>
2021-09-09 08:44:24 +0000

    新規カウンター作成ボタンの作成忘れ

--- app.rb
+++ app.rb
@@ -3,39 +3,53 @@ Bundler.require
 require 'sinatra/reloader' if development?
 require './models/count.rb'
 
+enable :sessions
+
 before do
   if Count.all.size == 0
     Count.create(number: 0)
   end
+
+  
 end
 
 get '/' do
-  @number = Count.first.number
+  Count.all.size.times do |n|#カラムの数だけ回す
+  eval("@number#{n+1}= Count.find(#{n+1}).number") 
+  end
    erb :index
 end
 
 get '/count' do
-  @number = Count.first.number
+  Count.all.size.times do |n|
+  eval("@number#{n+1}= Count.find(#{n+1}).number") #動的に今ある分だけ変数の生成
+  end
   erb :index
 end
 
-post '/plus' do
-  count = Count.first
+post '/plus/:id' do
+  count = Count.find(params[:id])
   count.number = count.number + 1
   count.save
   redirect '/count'
 end
 
-post '/minus' do
-  count = Count.first
+post '/minus/:id' do
+  count = Count.find(params[:id])
   count.number = count.number - 1
   count.save
   redirect '/count'
 end
 
-post "/clear" do
-  count=Count.first
+post "/clear/:id" do
+  count=Count.find(params[:id])
   count.number=0
   count.save
+  redirect "/count"
+end
+
+
+post"/new"do
+    Count.create(number: 0)
   redirect "/count"
 end
